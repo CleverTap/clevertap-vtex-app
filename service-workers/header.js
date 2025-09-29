52b15a9afc6ba82da3411b66b1214d9c30ce2438
Arthur Rodrigues <arthuroliveira@meto.do>
2025-09-29 15:57:53 -0300

    feat: add builder to initialize web-push worker

---
+++ service-workers/header.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+
+function exampleSWHeader() {
+  importScripts(
+    'https://s3-eu-west-1.amazonaws.com/static.wizrocket.com/js/sw_webpush.js'
+  )
+}
+
+export default exampleSWHeader
