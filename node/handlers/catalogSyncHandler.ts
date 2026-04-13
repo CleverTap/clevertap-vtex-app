ef1508026bf48d40d2f614e03143935fd25406ed
arthurrod <arthur9713@hotmail.com>
2026-04-13 14:11:37 -0300

    feat: add catalog timeout configuration and improve error handling

--- node/handlers/catalogSyncHandler.ts
+++ node/handlers/catalogSyncHandler.ts
@@ -4,15 +4,15 @@ export async function catalogSyncHandler(ctx: Context) {
   const { account } = ctx.vtex
   const { email, creator, replace } = ctx.body
 
-  ctx.status = 200
-  ctx.body = { message: `Load started` }
-
   const service = new CatalogService(ctx)
 
-  await service.syncCatalog(ctx, {
+  service.syncCatalog(ctx, {
     accountName: account,
     email,
     creator,
     replace,
   })
+
+  ctx.status = 200
+  ctx.body = { message: `Load started` }
 }
