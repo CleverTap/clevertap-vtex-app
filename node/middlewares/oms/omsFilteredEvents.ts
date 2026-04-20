a76f6b14b72bfce2a4d434ebb9d4be83ceb6c609
Manish Kumar <manish.kumar@clevertap.com>
2026-04-20 17:23:10 +0530

    fix compilation error

--- node/middlewares/oms/omsFilteredEvents.ts
+++ node/middlewares/oms/omsFilteredEvents.ts
@@ -189,13 +189,22 @@ async function handleCatalogSync(
 }
 
 async function getLastCatalogSync(vbaseClient: VBase) {
-  try {
-    return await vbaseClient.getJSON<string>('config', 'lastCatalogSync')
-  } catch (error) {
-    if (error?.response?.status === 404) {
-      return ''
-    }
+  let lastCatalogSync = ''
 
-    throw error
+  try {
+    lastCatalogSync = await vbaseClient.getJSON<string>(
+      'config',
+      'lastCatalogSync'
+    )
+  } catch (error) {
+    const errorString = error.toString()
+
+    if (errorString === 'Error: Request failed with status code 404') {
+      lastCatalogSync = new Date(0).toISOString()
+    } else {
+      throw error
+    }
   }
+
+  return lastCatalogSync
 }
